a75b61181b645eacb9c4593cc0b68df311365193
atbee <atb@odds.team>
2020-07-18 14:09:26 +0700

    Add maids model :bee:

--- homemaid/maids/tests.py
+++ homemaid/maids/tests.py
@@ -1,3 +1,26 @@
+from datetime import date
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Maid
+
+class TestMaid(TestCase):
+    def test_model_should_have_defined_fields(self):
+        # Given
+        Maid.objects.create(
+            name='Atb',
+            birthdate=date(1998, 4, 29),
+            description='Super Maid of the Year',
+            certificate='Best Maid 2020',
+            salary=3000
+        )
+
+        # When
+        maid = Maid.objects.last()
+        
+        # Then
+        self.assertEqual(maid.name, 'Atb')
+        self.assertEqual(maid.birthdate, date(1998, 4, 29))
+        self.assertEqual(maid.description, 'Super Maid of the Year')
+        self.assertEqual(maid.certificate, 'Best Maid 2020')
+        self.assertEqual(maid.salary, 3000)
