fca6aa95c5d2e4ed83f92fe1efc20e2e52a18e1e
atbee <atb@odds.team>
2020-07-18 15:38:51 +0700

    Use django extention and modified model using timestamped :bee:

--- homemaid/maids/tests/test_models.py
+++ homemaid/maids/tests/test_models.py
@@ -22,13 +22,13 @@ class TestMaid(TestCase):
         maid = Maid.objects.last()
         
         # Then
-        self.assertEqual(maid.name, 'Atb')
-        self.assertEqual(maid.birthdate, date(1998, 4, 29))
-        self.assertEqual(maid.description, 'Super Maid of the Year')
-        self.assertEqual(maid.certificate, 'Best Maid 2020')
-        self.assertEqual(maid.salary, 3000)
+        assert maid.name == 'Atb'
+        assert maid.birthdate == date(1998, 4, 29)
+        assert maid.description == 'Super Maid of the Year'
+        assert maid.certificate == 'Best Maid 2020'
+        assert maid.salary == 3000
 
-    def test_model_should_have_image_fields(self):
+    def test_model_should_have_image_field(self):
         # Given
         mock = MagicMock(spec=File)
         mock.name = 'profile.png'
@@ -46,6 +46,29 @@ class TestMaid(TestCase):
         maid = Maid.objects.last()
         
         # Then
-        self.assertEqual(maid.profile_image.name, 'profile.png')
+        assert maid.profile_image.name == 'profile.png'
+
+        os.remove('profile.png')
+
+    def test_model_should_have_created_and_modified_fields(self):
+        # Given
+        mock = MagicMock(spec=File)
+        mock.name = 'profile.png'
+
+        Maid.objects.create(
+            name='Atb',
+            profile_image=mock,
+            birthdate=date(1998, 4, 29),
+            description='Super Maid of the Year',
+            certificate='Best Maid 2020',
+            salary=3000
+        )
+
+        # When
+        maid = Maid.objects.last()
+        
+        # Then
+        assert maid.created is not None
+        assert maid.modified is not None
 
         os.remove('profile.png')
