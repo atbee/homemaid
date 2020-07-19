90feada87789063658da2e8a8b3bff3969346c48
atbee <atb@odds.team>
2020-07-19 13:11:02 +0700

    Init maid form :bee:

---
+++ homemaid/maids/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import Maid
+
+
+# class MaidForm(forms):
+#     name = forms.TextInput()
+#     ...
+
+class MaidForm(ModelForm):
+    class Meta:
+        model = Maid
+        fields = (
+            'name',
+        )
